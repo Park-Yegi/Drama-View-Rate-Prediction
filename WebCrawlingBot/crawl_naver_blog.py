ef7344eb7b30bca051b433499a7bd46f59054fa7
Park-Yegi <pyg9715@kaist.ac.kr>
2020-09-15 18:23:40 +0900

    Enable to crawl another form of blog

--- WebCrawlingBot/crawl_naver_blog.py
+++ WebCrawlingBot/crawl_naver_blog.py
@@ -26,10 +26,32 @@ def unconnect_to_db():
     connection.close()
 
 
+def sql_datetime_form(written_timestamp):
+    if (written_timestamp[-1] == '전'):
+        time_list = written_timestamp.split('시')
+        time_before = int(time_list[0])
+        now = time.localtime()
+        if (time_before < now.tm_hour):
+            written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour-time_before, now.tm_min, now.tm_sec)
+        else:
+            if (now.tm_mday != 1):
+                written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday-1, now.tm_hour-time_before+24, now.tm_min, now.tm_sec)
+            else:
+                if (now.tm_mon != 1):
+                    written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+                else:
+                    written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+    else: 
+        written_time = written_timestamp.replace('. ', '-', 2).replace('.', "") + ":00"
+
+    return written_time
+
+
 def search_article(drama_name, drama_id, pageNo):
     print("===== Search naver blog ", drama_name, " pageNo=", pageNo, " =====")
     links = []
-    # hdr = {'User-Agent': 'Mozilla/5.0'}
+    # hdr = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','Accept-Encoding': 'gzip, deflate, br','Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7','Cache-Control': 'max-age=0','Connection': 'keep-alive','Host': 'section.blog.naver.com','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
+    
     browser = webdriver.Chrome('./chromedriver')
     url = 'https://section.blog.naver.com/Search/Post.nhn?pageNo=' + str(pageNo) +'&rangeType=ALL&orderBy=sim&keyword='+drama_name
     browser.get(url)
@@ -49,6 +71,7 @@ def search_article(drama_name, drama_id, pageNo):
 
 def get_each_article(links, drama_id):
     try:
+        browser = webdriver.Chrome('./chromedriver')
         for link in links:
             print("===== Start to crawl ", link, " =====")
             url_hash = hashlib.sha256(link.encode()).hexdigest()
@@ -56,7 +79,6 @@ def get_each_article(links, drama_id):
                 print("Already crawled news")
                 continue
 
-            browser = webdriver.Chrome('./chromedriver')
             browser.get(link)
             browser.switch_to.frame('mainFrame')
             # hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -66,7 +88,7 @@ def get_each_article(links, drama_id):
             data = bs(source, 'html.parser')
             body = data.find('body')
             main = body.select_one('div#head-skin > div#body > div#whole-border > div#whole-body > div#wrapper > div#twocols > div#content-area > div > div > div > div > table > tbody > tr > td.bcc > div > div.se-viewer')
-            footer = body.select_one('div#head-skin > div#body > div#whole-border > div#whole-body > div#wrapper > div#twocols > div#content-area > div > div > div > div > table > tbody > tr > td.bcc > div > div.wrap_postcomment')
+            # footer = body.select_one('div#head-skin > div#body > div#whole-border > div#whole-body > div#wrapper > div#twocols > div#content-area > div > div > div > div > table > tbody > tr > td.bcc > div > div.wrap_postcomment')
             # footer = body.select_one('div#head-skin > div#body > div#whole-border > div#whole-body > div#wrapper > div#twocols > div#content-area > div > div > div > div > table > tbody > tr > td.bcc > div.post-btn > div.wrap_postcomment')
             if (main == None):
                 main = body.select_one('div#head-skin > div#body > div#whole-border > div#whole-body > div#wrapper > div#twocols > div#content-area > div > div > div > div > table.post-body > tbody > tr > td.bcc > div > div.view > div.se-viewer')
@@ -82,43 +104,26 @@ def get_each_article(links, drama_id):
                     body_text = body_text + text_piece.get_text()
                 # likes = footer.select('div.area_sympathy > a > div > span > em')[1].get_text()
                 # comments = int(footer.select_one('div.area_comment > a > em').get_text().strip())
-
-                if (written_timestamp[-1] == '전'):
-                    time_list = written_timestamp.split('시')
-                    time_before = int(time_list[0])
-                    now = time.localtime()
-                    if (time_before < now.tm_hour):
-                        written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour-time_before, now.tm_min, now.tm_sec)
-                    else:
-                        if (now.tm_mday != 1):
-                            written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday-1, now.tm_hour-time_before+24, now.tm_min, now.tm_sec)
-                        else:
-                            if (now.tm_mon != 1):
-                                written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-                            else:
-                                written_time = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-                else: 
-                    written_time = written_timestamp.replace('. ', '-', 2).replace('.', "") + ":00"
-
                     
                 # news_tuple = (drama_id, url_hash, title, written_time, body_text, likes, comments)
-                news_tuple = (drama_id, url_hash, title, written_time, body_text)
-                # print(drama_id)
-                # print(url_hash)
-                # print(title)
-                # print(written_time)
-                # print(body_text)
-                # print(likes)
-                # print(comments)
+                
 
-                try:
-                    # cursor.execute("INSERT INTO naver_blog(id, url_hash, title, modified_time, body_text, likes, comments) values (%s, %s, %s, %s, %s, %s, %s)", news_tuple)
-                    cursor.execute("INSERT INTO naver_blog(id, url_hash, title, modified_time, body_text) values (%s, %s, %s, %s, %s)", news_tuple)
-                except Exception as e:
-                    print(e)
+                # try:
+                #     # cursor.execute("INSERT INTO naver_blog(id, url_hash, title, modified_time, body_text, likes, comments) values (%s, %s, %s, %s, %s, %s, %s)", news_tuple)
+                    
+                # except Exception as e:
+                #     print(e)
                 
             else:
-                print("Cannot Crawl this naver blog: %s", link)
+                main = body.find_all('td', {'class':'bcc'})[1]
+                title = main.select_one('table.post-top > tbody > tr > td > div.htitle > span').get_text().strip()
+                written_timestamp = main.select_one('table.post-top > tbody > tr > td > p.date').get_text().strip()
+                body_text = main.select_one('div#postViewArea > div.post-view').get_text().strip()
+                
+
+            written_time = sql_datetime_form(written_timestamp)
+            news_tuple = (drama_id, url_hash, title, written_time, body_text)
+            cursor.execute("INSERT INTO naver_blog(id, url_hash, title, modified_time, body_text) values (%s, %s, %s, %s, %s)", news_tuple)
 
     except Exception as e:
         print(e)
@@ -128,11 +133,11 @@ def get_each_article(links, drama_id):
 
 if __name__ == "__main__":
     connect_to_db()
-    cursor.execute("SELECT id, drama_name from drama_info order by id desc")
+    cursor.execute("SELECT id, drama_name from drama_info order by drama_name desc")
     drama_id_name_list = cursor.fetchall()
 
     for drama in drama_id_name_list:
-        for pageNo in range(0, 10):
+        for pageNo in range(1, 10):
             links = search_article(drama[1], drama[0], pageNo)
             get_each_article(links, drama[0])
             connection.commit()
--- WebCrawlingBot/crawl_naver_news.py
+++ WebCrawlingBot/crawl_naver_news.py
@@ -48,6 +48,21 @@ def search_news(drama_name, drama_id, start):
     return links
 
 
+def sql_datetime_form(written_time):
+    new_written_time = written_time.replace('.', '-', 2).replace('.', "") + ":00"
+    timestamp_list = new_written_time.split(' ')
+    if (timestamp_list[1] == "오후"):
+        time_list = timestamp_list[2].split(":")
+        if (int(time_list[0]) != 12):
+            time_list[0] = str(int(time_list[0])+12)
+        else:
+            time_list[0] = str(int(time_list[0]))
+        timestamp_list[2] = ":".join(time_list)
+                    
+    new_written_time = timestamp_list[0] + ' ' + timestamp_list[2]
+    return new_written_time
+
+
 def get_each_news(links, drama_id):
     hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36', 'Content-Type': 'application/json; charset=utf-8'}
     # browser = webdriver.Chrome('./chromedriver')
@@ -74,6 +89,7 @@ def get_each_news(links, drama_id):
         data = bs(source, 'html.parser')
         body = data.find('body')
         main = body.select_one('div#wrap > div#content > div.end_ct > div.end_ct_area')
+            
         
         try:
             if (main != None):
@@ -87,18 +103,31 @@ def get_each_news(links, drama_id):
                 # print(written_time)
                 # print(body_text)
                 # print(recommends)
-                written_time = written_time.replace('.', '-', 2).replace('.', "") + ":00"
-                timestamp_list = written_time.split(' ')
-                if (timestamp_list[1] == "오후"):
-                    time_list = timestamp_list[2].split(":")
-                    if (int(time_list[0]) != 12):
-                        time_list[0] = str(int(time_list[0])+12)
-                    else:
-                        time_list[0] = str(int(time_list[0]))
-                    timestamp_list[2] = ":".join(time_list)
+
+                # written_time = written_time.replace('.', '-', 2).replace('.', "") + ":00"
+                # timestamp_list = written_time.split(' ')
+                # if (timestamp_list[1] == "오후"):
+                #     time_list = timestamp_list[2].split(":")
+                #     if (int(time_list[0]) != 12):
+                #         time_list[0] = str(int(time_list[0])+12)
+                #     else:
+                #         time_list[0] = str(int(time_list[0]))
+                #     timestamp_list[2] = ":".join(time_list)
                     
-                written_time = timestamp_list[0] + ' ' + timestamp_list[2]
-                news_tuple = (drama_id, url_hash, title, written_time, body_text, recommends)
+                # written_time = timestamp_list[0] + ' ' + timestamp_list[2]
+                new_written_time = sql_datetime_form(written_time)
+                news_tuple = (drama_id, url_hash, title, new_written_time, body_text, recommends)
+
+                cursor.execute("INSERT INTO naver_news(id, url_hash, title, modified_time, body_text, recommends) values (%s, %s, %s, %s, %s, %s)", news_tuple)
+            else:
+                main = body.select_one('div#wrap > table.container > tr > td.content > div#main_content')
+                title = main.select_one('div.article_header > div.article_info > h3').get_text()
+                written_time = main.select_one('div.article_header > div.article_info > div.sponsor > span').get_text()
+                body_text = main.select_one('div#articleBody > div#articleBodyContents').get_text().strip()
+                # recommends = int(main.select_one('div#articleBody > div.end_bnt > div.tomain > div > a > em.u_cnt._count').get_text())
+
+                new_written_time = sql_datetime_form(written_time)
+                news_tuple = (drama_id, url_hash, title, new_written_time, body_text, recommends)
 
                 cursor.execute("INSERT INTO naver_news(id, url_hash, title, modified_time, body_text, recommends) values (%s, %s, %s, %s, %s, %s)", news_tuple)
         
@@ -108,7 +137,7 @@ def get_each_news(links, drama_id):
 
 if __name__ == "__main__":
     connect_to_db()
-    cursor.execute("SELECT id, drama_name from drama_info")
+    cursor.execute("SELECT id, drama_name from drama_info order by drama_name desc")
     drama_id_name_list = cursor.fetchall()
 
     for drama in drama_id_name_list:
